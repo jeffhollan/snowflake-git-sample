80899263b20f6622e8f715cad5b9fec7a0902884
Jeff Hollan <jeff.hollan@snowflake.com>
2024-04-02 21:47:41 -0700

    fixing snowpark code

--- snowpark/app.py
+++ snowpark/app.py
@@ -1,6 +1,11 @@
 from snowflake.snowpark import Session
 
 
-def return_simple_table(Session: session):
+def return_simple_table(session):
     df = session.table("simple_table").collect()
     return df
+
+
+if __name__ == "__main__":
+    session = Session.builder.getOrCreate()
+    return_simple_table(session).show()
